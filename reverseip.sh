958649f9a40a9106c65bbe298a7316d7773e0194
florienzh4x <hasanal.bulkiah501@gmail.com>
2018-12-26 02:28:50 +0700

    Update reverseip.sh

--- reverseip.sh
+++ reverseip.sh
@@ -1,51 +1,40 @@
 #!/bin/bash
 
 yougetsignal(){
-	web="$1"
-	printf "    YouGetSignal... "
-	curl=$(curl -s -F "remoteAddress=$web" "https://domains.yougetsignal.com/domains.php")
-	if [[ $curl =~ "status\":\"Success" ]]; then
-		domain=$(echo $curl | grep -Po '\["\K.*?(?=",)')
-		echo "$domain" >> result.tmp
-		total=$(echo $curl | grep -Po '(?<=domainCount":")[^",]*')
+	web="${1}"
+	printf "[!] ${korban} YouGetSignal... "
+	curl=$(curl -s -F "remoteAddress=${web}" "https://domains.yougetsignal.com/domains.php" | grep -Po '\["\K.*?(?=",)')
+	if [[ ! $curl == '' ]]; then
+		total=$(echo $curl | sed 's/ /\n/g' | wc -l)
+		echo "$curl" >> result.tmp
 		printf "$total Domains\n"
-	elif [[ $curl =~ "Service unavailable" ]]; then
-		curl2=$(curl -s -F "remoteAddress=$web" "https://domains.yougetsignal.com/domains.php")
-		if [[ $curl2 =~ "status\":\"Success" ]]; then
-			domain2=$(echo $curl2 | grep -Po '\["\K.*?(?=",)')
-			echo "$domain2" >> result.tmp
-			total2=$(echo $curl2 | grep -Po '(?<=domainCount":")[^",]*')
-			printf "$total2 Domains\n"
-		else
-			printf "\n"
-		fi		
-	elif [[ $curl =~ "check limit reached" ]]; then
-		printf "[LIMIT]\n"
 	else
-		printf "[UNKNOWN]\n"
+		printf "0 Domains\n"
 	fi
 }
 viewdns(){
-	web="$1"
-	printf "    ViewDNS... "
-	i=0;
-	for ResWeb in $(curl -s "https://viewdns.info/reverseip/?host=$web&t=1" | grep -Po '(?<=<td>)[[:alnum:]_.-]+?\.[[:alpha:].]{2,10}[^</td]*');
-	do
-		((i++))
-		echo "$ResWeb" >> result.tmp
-	done
-	printf "$i Domains\n"
+	web="${1}"
+	printf "[!] ${korban} ViewDNS... "
+	curl=$(curl -s "https://viewdns.info/reverseip/?host=${web}&t=1" -L -D - | grep -Po '(?<=<td>)[[:alnum:]_.-]+?\.[[:alpha:].]{2,10}[^</td]*')
+	if [[ ! $curl == '' ]]; then
+		total=$(echo $curl | sed 's/ /\n/g' | wc -l)
+		echo "$curl" >> result.tmp
+		printf "$total Domains\n"
+	else
+		printf "0 Domains\n"
+	fi
 }
 hackertarget() {
-	web="$1"
-	printf "    HackerTarget... "
-	i=0;
-	for SITE in $(curl -s -d "theinput=$web&thetest=reverseiplookup&name_of_nonce_field=d210302267&_wp_http_referer=%2Freverse-ip-lookup%2F" "https://hackertarget.com/reverse-ip-lookup/" | sed -n -e '/<pre id="formResponse">/,/<\/pre>/p' | grep -Po '([[:alnum:]_.-]+?\.){1,5}[[:alpha:].]{2,10}')
-	do
-		((i++))
-		echo "${SITE}" >> result.tmp
-	done
-	printf "$i Domains\n"
+	web="${1}"
+	printf "[!] ${korban} HackerTarget... "
+	curl=$(curl -s -d "theinput=${web}&thetest=reverseiplookup&name_of_nonce_field=d210302267&_wp_http_referer=%2Freverse-ip-lookup%2F" "https://hackertarget.com/reverse-ip-lookup/" | sed -n -e '/<pre id="formResponse">/,/<\/pre>/p' | grep -Po '([[:alnum:]_.-]+?\.){1,5}[[:alpha:].]{2,10}')
+	if [[ ! $curl == '' ]]; then
+		total=$(echo $curl | sed 's/ /\n/g' | wc -l)
+		echo "$curl" >> result.tmp
+		printf "$total Domains\n"
+	else
+		printf "0 Domains\n"
+	fi
 }
 
 
@@ -55,17 +44,21 @@ if [[ -z $1 ]]; then
 fi
 
 sed -i 's/\r//' $1
-
 IFS=$'\r\n' GLOBIGNORE='*' command eval  'target=($(cat $1))'
 for (( i = 0; i <"${#target[@]}"; i++ )); do
 	korban=$(echo "${target[$i]}" | grep -Po '([[:alnum:]_.-]+?\.){1,5}[[:alpha:].]{2,10}')
 
-	printf "[!] $korban\n"
-	yougetsignal $korban;
-	hackertarget $korban;
-	viewdns $korban;
+	ngirim=`expr $i % 20`
+	if [[ $ngirim == 0 && $i > 0 ]]; then
+		sleep 5
+	fi
+
+	yougetsignal ${korban};
+	hackertarget ${korban};
+	viewdns ${korban};
 done
 wait
-printf "\n[!] Filtering Result... "
+printf "\n[!] Filtering Result... \n"
 cat result.tmp | sort -u | uniq >> result.txt
+rm -rf result.tmp
 printf "[!] Total : $(cat result.txt | wc -l) Domains\n\n"
